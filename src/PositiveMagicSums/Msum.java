b24ec54e6b925b4b5ecffd35b3a64fc08f63c69e
Barbara Burakowska <bnburakowska@gmail.com>
2023-01-10 18:26:58 +0100

    task 2

---
+++ src/PositiveMagicSums/Msum.java
@@ -0,0 +1,22 @@
+package PositiveMagicSums;
+/**
+    An array of integers, arr[n], can be rearranged arbitraily. The magicsum at index i is defined as
+    msum[i] = arr[0] + arr[1] + ... + arr[i]
+    Rearange array and maximize the number of elements greater than 0 in the msum array.
+    Report the most number of elements greater than 0 out of the array msum
+
+    Note: here, a positive value is defined as an integer value.
+    Example:
+    n = 4,
+    arr = [-6,3,4,-10]
+    One optional arrangement is [3,4,-6,-10]. This has an array of magic sums msum = [3,7,1,-9] with 3 numbers grater than 0.
+    Return 3 as there is no way to have more than 3 positive elements.
+
+    [2,0,1,-3] -> [2,2,3,0]
+
+    [-3,0,-2] -> xxx
+
+**/
+
+public class Msum {
+}
